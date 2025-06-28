b9a4e6825ac7453160f21134279b910e7250b0c1
Shuuma21 <fwwzdzkirynfi@gmail.com>
2025-06-28 12:20:01 +0700

    memodifikasi file main.py di lokal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 nama = "zaki"
 umur = 19
+semester = "semester 2"
 
-print(f"nama saya adalah {nama} umur saya {umur}")
+print(f"nama saya adalah {nama} umur saya {umur} saya adalah mahasiswa {semester}")
